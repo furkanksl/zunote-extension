02d34c95da300a546a2d305f9b30491b7ab164ab
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-09 15:19:50 +0300

    Create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { SaveButton } from './components/SaveButton';
+import userLogo from './assets/icons/user.png'
+import reminderLogo from './assets/icons/reminder.png'
+import categoryLogo from './assets/icons/category.png'
+import { DateTimePicker } from './components/DateTimePicker';
 
 function App() {
+
+  const [isReminderSelected , setIsReminderSelected] = useState(false);
+  const [isCategorySelected , setIsCategorySelected] = useState(false);
+
+  function toggleReminder(){
+    setIsReminderSelected(!isReminderSelected);
+    setIsCategorySelected(false);
+  }
+
+  function toggleCategory(){
+    setIsCategorySelected(!isCategorySelected);
+    setIsReminderSelected(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="header">
+          <p>Notes</p>
+          <div className="profile">
+            <img src={userLogo} alt="Profile" title='Profile'/>
+          </div>
+        </div>
+
+        <div className="functions-section">
+          <div className="reminder" onClick={toggleReminder}>
+            <img src={reminderLogo} alt="Reminder" title='Set Reminder'/>
+          </div>
+          <div className="category" onClick={toggleCategory}>
+            <img src={categoryLogo} alt="Category" title='Set Category'/>
+            </div>
+        </div>
+        {isReminderSelected ?  <DateTimePicker /> : null}
+        <div className="text-field">
+          <textarea rows={3}></textarea>
+          <SaveButton />
+        </div>
+      </div>
     </div>
   );
 }
