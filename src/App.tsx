d018661e686ec6f29380b53755e97d7c670e8280
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-10 23:17:27 +0300

    Add routing conf

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import { HomePage } from "./pages/home/HomePage";
+import { LoginPage } from "./pages/login/LoginPage";
+// import { HomePage } from "./pages/home/HomePage";
 
 function App() {
     return (
         <div className="App">
-            <HomePage />
+            <Routes>
+                <Route path="*" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/home" element={<HomePage />} />
+            </Routes>
         </div>
     );
 }
