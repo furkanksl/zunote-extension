1f407a09ace4ad5607a05ab2174570a578a3fba3
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-10 16:41:43 +0300

    Update home page w/ showing selected category&date

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,21 +1,56 @@
 import * as React from "react";
 import "./HomePage.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { DateTimePicker } from "../../components/DateTimePicker";
 import { Header } from "../../components/Header";
 import { NoteField } from "../../components/NoteField";
 import { Category } from "../../components/Category";
+import closeIcon from "../../assets/icons/close-svgrepo-com.svg";
+import moment from "moment";
+import { unsetReminder } from "../../redux/feature/reminder.reducer";
+import { unselectCategory } from "../../redux/feature/category.reducer";
 
 type Props = {};
 export function HomePage(props: Props) {
+    const dispatch = useDispatch();
+
     const isReminderSelected = useSelector((state: any) => state.reminder.value);
     const isCategorySelected = useSelector((state: any) => state.category.value);
+    const reminderDate = useSelector((state: any) => state.reminder.reminderDate);
+    const selectedCategory = useSelector((state: any) => state.category.selectedCategory);
+    var dateString = moment.unix(reminderDate).format("MMM DD YYYY HH:MM");
+
+    function removeReminder() {
+        dispatch(unsetReminder());
+    }
+
+    function removeCategory() {
+        dispatch(unselectCategory());
+    }
 
     return (
         <div className="wrapper">
             <Header />
             {isReminderSelected ? <DateTimePicker /> : null}
             {isCategorySelected ? <Category /> : null}
+            <div className="info-container">
+                {reminderDate !== 0 ? (
+                    <div className="reminder-section">
+                        <p>Remind me : {dateString}</p>
+                        <img src={closeIcon} alt="close" onClick={removeReminder} />
+                    </div>
+                ) : (
+                    ""
+                )}
+                {selectedCategory !== "" ? (
+                    <div className="category-section">
+                        <p>Category : {selectedCategory}</p>
+                        <img src={closeIcon} alt="close" onClick={removeCategory} />
+                    </div>
+                ) : (
+                    ""
+                )}
+            </div>
             <NoteField />
         </div>
     );
