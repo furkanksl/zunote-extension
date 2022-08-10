4485ab1b9be7c2d86cb17250782cc809ea75233b
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-10 12:43:24 +0300

    Update datetime picker component

--- src/components/DateTimePicker.tsx
+++ src/components/DateTimePicker.tsx
@@ -1,75 +1,21 @@
 import * as React from 'react';
 import './DateTimePicker.scss';
-import 'rmc-picker/assets/index.css';
-import 'rmc-date-picker/assets/index.css';
-
-import DatePicker from 'rmc-date-picker/lib/DatePicker';
-import enUs from 'rmc-date-picker/lib/locale/en_US';
-
-export const minDate = new Date();
-export const maxDate = new Date(2025, 12, 1, 23, 59, 59);
-export let now = new Date();
-
-export function format(date:any) {
-  let mday = date.getDate();
-  let month = date.getMonth() + 1;
-  month = month < 10 ? `0${month}` : month;
-  mday = mday < 10 ? `0${mday}` : mday;
-  return `${date.getFullYear()}-${month}-${mday} ${date.getHours()}:${date.getMinutes()}`;
-}
+import  Datetime from 'react-datetime';
+import "react-datetime/css/react-datetime.css";
+import  { Moment } from 'moment';
 
 type Props = {
   
 };
 export function DateTimePicker(props: Props) {
-    let date = null;
-
-
-  const onDateChange = (date:any) => {
-    console.log('onChange', format(date));
-  
+  function valid(current:Moment) {
+    let date = new Date();
+    return current.toDate() >= date;
   }
-
-  const onValueChange = (values:any, index:any) => {
-    console.log('onValueChange', values, index);
-  }
-
-  const onScrollChange = (values:any, index:any) => {
-    console.log('onScrollChange', values, index);
-  }
-
-  const changeMode = (e:any) => {
-    
-  }
-
-  const datePicker = (
-    <DatePicker
-      rootNativeProps={{'data-xx': 'yy'}}
-      minDate={minDate}
-      maxDate={maxDate}
-      defaultDate={now}
-      mode={'datetime'}
-      locale={enUs}
-    />
-  );
 
   return (
     <div className='datetime-picker-container'>
-        <span>{date && format(date) || format(now)}</span>
-        <div>
-            <DatePicker
-            rootNativeProps={{'data-xx': 'yy'}}
-            defaultDate={date || now}
-            mode={'datetime'}
-            locale={enUs}
-            maxDate={maxDate}
-            minDate={minDate}
-            onDateChange={onDateChange}
-            onValueChange={onValueChange}
-            onScrollChange={onScrollChange}
-            use12Hours
-            />
-        </div>
+      <Datetime isValidDate={valid} dateFormat="YYYY-MMM-D"  initialValue={new Date()} inputProps={{readOnly:true}}/>
     </div>
   );
 };
