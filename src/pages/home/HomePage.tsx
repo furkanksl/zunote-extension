519891fecfe1e858d9b4b86737e9533a3116b874
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-10 15:13:42 +0300

    Update home page ui

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,15 +4,18 @@ import { useSelector } from "react-redux";
 import { DateTimePicker } from "../../components/DateTimePicker";
 import { Header } from "../../components/Header";
 import { NoteField } from "../../components/NoteField";
+import { Category } from "../../components/Category";
 
 type Props = {};
 export function HomePage(props: Props) {
     const isReminderSelected = useSelector((state: any) => state.reminder.value);
+    const isCategorySelected = useSelector((state: any) => state.category.value);
 
     return (
         <div className="wrapper">
             <Header />
             {isReminderSelected ? <DateTimePicker /> : null}
+            {isCategorySelected ? <Category /> : null}
             <NoteField />
         </div>
     );
