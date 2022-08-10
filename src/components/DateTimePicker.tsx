c744a79745995c63501dee59635c09865d2afebe
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-10 16:40:51 +0300

    Add select date functioanlity

--- src/components/DateTimePicker.tsx
+++ src/components/DateTimePicker.tsx
@@ -1,21 +1,36 @@
-import * as React from 'react';
-import './DateTimePicker.scss';
-import  Datetime from 'react-datetime';
+import * as React from "react";
+import "./DateTimePicker.scss";
+import Datetime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
-import  { Moment } from 'moment';
+import { Moment } from "moment";
+import { useDispatch } from "react-redux";
+import { setReminder } from "../redux/feature/reminder.reducer";
 
-type Props = {
-  
-};
+type Props = {};
 export function DateTimePicker(props: Props) {
-  function valid(current:Moment) {
-    let date = new Date();
-    return current.toDate() >= date;
-  }
+    const dispatch = useDispatch();
 
-  return (
-    <div className='datetime-picker-container'>
-      <Datetime isValidDate={valid} dateFormat="YYYY-MMM-D"  initialValue={new Date()} inputProps={{readOnly:true}}/>
-    </div>
-  );
-};
+    function valid(current: Moment) {
+        let date = new Date();
+        return current.toDate() >= date;
+    }
+
+    function onChangeDatetime(val: any) {
+        console.log(val.unix());
+
+        dispatch(setReminder(val.unix()));
+    }
+
+    return (
+        <div className="datetime-picker-container">
+            <Datetime
+                isValidDate={valid}
+                dateFormat="YYYY-MMM-D"
+                closeOnClickOutside={true}
+                onChange={(val) => onChangeDatetime(val)}
+                initialValue={new Date()}
+                inputProps={{ readOnly: true }}
+            />
+        </div>
+    );
+}
