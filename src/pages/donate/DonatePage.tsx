cfc065bcaceb2c1a86593f641878ec5b3da17345
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-11 22:58:02 +0300

    Finalize donate page ui

---
+++ src/pages/donate/DonatePage.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import "./DonatePage.scss";
+import { PageHeader } from "../../components/PageHeader";
+
+type Props = {};
+export function DonatePage(props: Props) {
+    return (
+        <div className="wrapper">
+            <PageHeader title="DONATE" />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus illum ullam, aliquam animi
+                doloribus iure voluptas corporis quae deserunt nobis, ipsam ad dignissimos eum tempora aut dolore
+                aliquid nostrum reiciendis!
+            </p>
+            <a href="https://www.buymeacoffee.com/furkankoseoglu" target="_blank" className="buy-me-a-coffe-button">
+                <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" />
+            </a>
+        </div>
+    );
+}
