3ae37ad498fcc662ca9decd711ec4fcaa3146f84
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-11 20:07:21 +0300

    Update about page with header component

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { BackButton } from "../../components/BackButton";
+import { PageHeader } from "../../components/PageHeader";
 import styles from "./AboutPage.module.scss";
 
 type Props = {};
 export function AboutPage(props: Props) {
     return (
         <div className="wrapper">
-            <div className={styles.header}>
-                <BackButton />
-                <p className={styles.title}>ABOUT</p>
-                <div className={styles.empty}></div>
-            </div>
-
+            <PageHeader title="ABOUT" />
             <p>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus eos ea necessitatibus esse
                 accusantium architecto aliquam sapiente ullam enim dolor, officia quae nobis doloribus distinctio
