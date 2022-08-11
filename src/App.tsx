7afc6d57bc88bb0d71e96838762be59b3308646f
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-08-11 21:07:13 +0300

    Update routing and Fix textfield nullable prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import { AboutPage } from "./pages/about/AboutPage";
+import { FeedbackPage } from "./pages/feedback/FeedbackPage";
 import { HomePage } from "./pages/home/HomePage";
 import { LoginPage } from "./pages/login/LoginPage";
 import { ProfilePage } from "./pages/profile/ProfilePage";
@@ -10,13 +11,14 @@ function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/*" element={<Navigate to="login" replace />} />
                 <Route caseSensitive={true} path="login" element={<LoginPage />} />
                 <Route caseSensitive={true} path="home" element={<HomePage />} />
                 <Route path="profile/*">
                     <Route path="" element={<ProfilePage />} />
                     <Route caseSensitive={true} path="about" element={<AboutPage />} />
+                    <Route caseSensitive={true} path="feedback" element={<FeedbackPage />} />
                 </Route>
+                <Route path="/*" element={<Navigate to="login" replace />} />
             </Routes>
         </div>
     );
--- src/components/TextFieldWithButton.tsx
+++ src/components/TextFieldWithButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 };
 export function TextFieldWithButton(props: Props) {
     return (
-        <div className="text-field" style={props.styles.textfield}>
+        <div className="text-field" style={props?.styles?.textfield}>
             <textarea rows={3} placeholder={props.placeholder}></textarea>
             <SaveButton buttonName={props.buttonText} />
         </div>
