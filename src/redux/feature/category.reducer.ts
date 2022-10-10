3a95a64637a09d1f84bd0a63f8f7137b57528377
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-10 04:19:15 +0300

    Update category reducer with category list

--- src/redux/feature/category.reducer.ts
+++ src/redux/feature/category.reducer.ts
@@ -1,29 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const categorySlice = createSlice({
-  name: 'category',
-  initialState: {
-    value: false,
-    selectedCategory: '',
-  },
-  reducers: {
-    toggleCategory: (state) => {
-      state.value = !state.value;
+    name: "category",
+    initialState: {
+        value: false,
+        selectedCategory: "",
+        categories: [],
     },
-    setCategoryState: (state, action) => {
-      state.value = action.payload
+    reducers: {
+        setCategories: (state, action) => {
+            state.categories = action.payload;
+        },
+
+        toggleCategory: (state) => {
+            state.value = !state.value;
+        },
+
+        setCategoryState: (state, action) => {
+            state.value = action.payload;
+        },
+
+        unselectCategory: (state) => {
+            state.selectedCategory = "";
+        },
+
+        setSelectedCategory: (state, action) => {
+            state.selectedCategory = action.payload;
+        },
     },
-    unselectCategory: (state) => {
-      state.selectedCategory = '';
-    },
-    setSelectedCategory: (state, action) => {
-      state.selectedCategory = action.payload
-    },
-  
-  },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { toggleCategory, setCategoryState, unselectCategory, setSelectedCategory } = categorySlice.actions
+export const { setCategories, toggleCategory, setCategoryState, unselectCategory, setSelectedCategory } =
+    categorySlice.actions;
 
-export default categorySlice.reducer
+export default categorySlice.reducer;
