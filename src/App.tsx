0e0a03c139b3f117eeaf5bb1b88b29eb3d8753a3
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-10 03:35:29 +0300

    Update app with protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.scss";
+
+import { Navigate, Route, Routes } from "react-router-dom";
 import { AboutPage } from "./pages/about/AboutPage";
 import { DonatePage } from "./pages/donate/DonatePage";
 import { FeedbackPage } from "./pages/feedback/FeedbackPage";
 import { HomePage } from "./pages/home/HomePage";
-import { LoginPage } from "./pages/login/LoginPage";
+import { AuthPage } from "./pages/auth/AuthPage";
 import { ProfilePage } from "./pages/profile/ProfilePage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+    function protectRoute(child: any) {
+        return <ProtectedRoute>{child}</ProtectedRoute>;
+    }
+
     return (
         <div className="App">
             <Routes>
-                <Route caseSensitive={true} path="login" element={<LoginPage />} />
-                <Route caseSensitive={true} path="home" element={<HomePage />} />
+                <Route caseSensitive={true} path="auth" element={<AuthPage />} />
+                <Route caseSensitive={true} path="home" element={protectRoute(<HomePage />)} />
+
                 <Route path="profile/*">
-                    <Route path="" element={<ProfilePage />} />
-                    <Route caseSensitive={true} path="about" element={<AboutPage />} />
-                    <Route caseSensitive={true} path="feedback" element={<FeedbackPage />} />
-                    <Route caseSensitive={true} path="donate" element={<DonatePage />} />
+                    <Route path="" element={protectRoute(<ProfilePage />)} />
+                    <Route caseSensitive={true} path="about" element={protectRoute(<AboutPage />)} />
+                    <Route caseSensitive={true} path="feedback" element={protectRoute(<FeedbackPage />)} />
+                    <Route caseSensitive={true} path="donate" element={protectRoute(<DonatePage />)} />{" "}
                 </Route>
-                <Route path="/*" element={<Navigate to="login" replace />} />
+
+                <Route path="/*" element={<Navigate to="/home" replace />} />
             </Routes>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
+import "react-toastify/dist/ReactToastify.css";
+
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <Provider store={store}>
         <BrowserRouter>
             <App />
+
+            <ToastContainer theme="colored" autoClose={2500} limit={1} />
         </BrowserRouter>
     </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
