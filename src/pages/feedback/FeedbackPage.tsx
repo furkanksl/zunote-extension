27acbb5fd4b86ec6e796fe9da38a10ee6c25dfda
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-10 04:21:01 +0300

    Update feedback page to be able to send feedback

--- src/pages/feedback/FeedbackPage.tsx
+++ src/pages/feedback/FeedbackPage.tsx
@@ -5,13 +5,18 @@ import { PageHeader } from "../../components/Header/PageHeader";
 import { TextFieldWithButton } from "../../components/Form/TextFieldWithButton";
 
 import feedbackIcon from "../../assets/icons/feedback.png";
+import FirebaseService from "../../services/firebase/firebase.service";
 
 export function FeedbackPage() {
+    const firebaseService = new FirebaseService();
+
+    const sendFeedback = async (e: any, feedback: string) => await firebaseService.sendFeedback(feedback);
+
     return (
         <div className="wrapper">
             <PageHeader title="FEEDBACK" />
             <img className={styles.feedbackIcon} src={feedbackIcon} alt="feedback-icon" />
-            <TextFieldWithButton placeholder="Let us know the ..." buttonText="SEND" />
+            <TextFieldWithButton onClick={sendFeedback} placeholder="Let us know the ..." buttonText="SEND" />
         </div>
     );
 }
