ec052939d79107476e351b10dd05d88f85db8918
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-10 03:33:25 +0300

    Create a reducer for auth ops

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
-import categoryReducer from './feature/category.reducer'
-import reminderReducer from './feature/reminder.reducer'
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./feature/auth.reducer";
+import categoryReducer from "./feature/category.reducer";
+import reminderReducer from "./feature/reminder.reducer";
 
 export default configureStore({
-  reducer: {
-    reminder : reminderReducer,
-    category : categoryReducer
-  },
-})
+    reducer: {
+        reminder: reminderReducer,
+        category: categoryReducer,
+        auth: authReducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware({
+            serializableCheck: false,
+        });
+    },
+});
