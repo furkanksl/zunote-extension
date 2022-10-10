20205073972013389d69bee18a88e229fc35e055
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-10 04:20:28 +0300

    Update form field functionaity

--- src/components/Form/SaveButton.tsx
+++ src/components/Form/SaveButton.tsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
+import LoadingSvgComponent from "../Svg/LoadingSvg";
 import "./SaveButton.scss";
 
 type Props = {
     buttonName: string;
+    onClick: (e: any) => any;
 };
 export function SaveButton(props: Props) {
-    function onSave(e: any) {
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function onSave(e: any) {
         e.preventDefault();
+
+        setIsLoading(true);
+
+        try {
+            await props.onClick(e);
+        } catch (error) {
+            console.log(error);
+        }
+
+        setIsLoading(false);
     }
 
     return (
         <div>
-            <button className="button" onClick={onSave}>
-                <span>{props.buttonName}</span>
+            <button className="button" onClick={async (e: any) => onSave(e)}>
+                {isLoading ? <LoadingSvgComponent /> : <span>{props.buttonName}</span>}
             </button>
         </div>
     );
--- src/components/Form/TextFieldWithButton.tsx
+++ src/components/Form/TextFieldWithButton.tsx
@@ -1,16 +1,39 @@
 import "./TextFieldWithButton.scss";
 import { SaveButton } from "./SaveButton";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 type Props = {
     buttonText: string;
+    onClick: (event: any, text: string) => any;
     placeholder: string;
     styles?: any;
 };
 export function TextFieldWithButton(props: Props) {
+    const [inputValue, setInputValue] = useState("");
+
+    function handleMessageChange(event: any) {
+        setInputValue(event.target.value);
+    }
+
+    function onSave(e: any) {
+        if (!inputValue) {
+            toast.info("Please fill the field!");
+            return;
+        }
+        props.onClick(e, inputValue);
+        setInputValue("");
+    }
+
     return (
         <div className="text-field" style={props?.styles?.textfield}>
-            <textarea rows={3} placeholder={props.placeholder}></textarea>
-            <SaveButton buttonName={props.buttonText} />
+            <textarea
+                rows={3}
+                value={inputValue}
+                placeholder={props.placeholder}
+                onChange={handleMessageChange}
+            ></textarea>
+            <SaveButton buttonName={props.buttonText} onClick={(e: any) => onSave(e)} />
         </div>
     );
 }
